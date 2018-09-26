60116d01c8fd3533d6230b8b3fa7a1b19d9521e0
Anna Verkhovskaya <verkhovskaya.anna@gmail.com>
2018-09-26 00:36:35 -0400

    Changed absolute paths to relative paths

--- server_main.py
+++ server_main.py
@@ -2,9 +2,10 @@ from bottle import route, request, static_file, run, post
 import uuid
 import sys
 import time
-
+import os
 
 sys.setrecursionlimit(100000)
+root_path = str(os.getcwd())
 #source: google
 #info in: None
 #info out: session id
@@ -14,60 +15,59 @@ sys.setrecursionlimit(100000)
 def root():
 	session_id = str(uuid.uuid4())
 
-	root = "/Users/2017-A/Dropbox/web_dev/modelcraft"
 
-	text = open(root + "/html/header.html", "r").read() + \
-		   open(root + "/html/session_id.html", "r").read().replace("$$session_id$$", session_id) + \
-		   open(root + "/html/0_description.html", "r").read() + \
-		   open(root + "/html/1_upload_map.html", "r").read() + \
-		   open(root + "/html/2_pick_location.html", "r").read() + \
-		   open(root + "/html/3_results.html", "r").read() + \
-		   open(root + "/html/footer.html", "r").read()
+	text = open(root_path + "/html/header.html", "r").read() + \
+		   open(root_path + "/html/session_id.html", "r").read().replace("$$session_id$$", session_id) + \
+		   open(root_path + "/html/0_description.html", "r").read() + \
+		   open(root_path + "/html/1_upload_map.html", "r").read() + \
+		   open(root_path + "/html/2_pick_location.html", "r").read() + \
+		   open(root_path + "/html/3_results.html", "r").read() + \
+		   open(root_path + "/html/footer.html", "r").read()
 	return text
 
 @post('/request_render')
 def request_render():
 	print(request.files.__dict__)
-	time.sleep(5)
+	time.sleep(3)
 
 #----------------------- Serve files
 
 
 @route('/favicon')
 def favicon():
-	return static_file("favicon.jpeg", root="/Users/2017-A/Dropbox/web_dev/modelcraft")
+	return static_file("favicon.jpeg", root=root_path)
 
 @route('/show_layout_pdf/<session_id>')
 def serve_layout_pdf(session_id):
-	return static_file("layout.pdf", root="/Users/2017-A/Dropbox/web_dev/modelcraft")
+	return static_file("layout.pdf", root=root_path)
 
 @route('/show_cutout_pdf/<session_id>')
 def serve_layout_pdf(session_id):
-	return static_file("layout.pdf", root="/Users/2017-A/Dropbox/web_dev/modelcraft")
+	return static_file("layout.pdf", root=root_path)
 
 @route('/download_layout_pdf/<session_id>')
 def serve_layout_pdf(session_id):
-	return static_file("layout.pdf", root="/Users/2017-A/Dropbox/web_dev/modelcraft/data/" + session_id, download="layout.pdf")
+	return static_file("layout.pdf", root=root_path + "/data/" + session_id, download="layout.pdf")
 
 @route("/download_dxf/<session_id>")
 def dxf(session_id):
-	return static_file("cutout.dxf", root="/Users/2017-A/Dropbox/web_dev/modelcraft/data/"+session_id, download="cutout.dxf")
+	return static_file("cutout.dxf", root=root_path + "/data/"+session_id, download="cutout.dxf")
 
 @route('/get_stylesheet')
 def stylesheet():
-	return static_file("stylesheet.css", root="/Users/2017-A/Dropbox/web_dev/modelcraft/css")
+	return static_file("stylesheet.css", root=root_path + "/css")
 
 @route('/javascript_functions')
 def stylesheet():
-	return static_file("script.js", root="/Users/2017-A/Dropbox/web_dev/modelcraft/javascript")
+	return static_file("script.js", root=root_path + "/javascript")
 
 @route('/serve_icon/<session_id>')
 def serve_icon(session_id):
-	return static_file("icon.png", root="/Users/2017-A/Dropbox/web_dev/modelcraft/data/" + session_id + "/map")
+	return static_file("icon.png", root=root_path + "/data/" + session_id + "/map")
 
 @route('/root_image')
 def root_image():
-	return static_file("house.jpeg", root="/Users/2017-A/Dropbox/web_dev/modelcraft")
+	return static_file("house.jpeg", root=root_path)
 
 
 try:
